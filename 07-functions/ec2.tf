82dab57af8abacaef67d59be4045dd91bd9f15db
GopalIndla <gopal.prnl7@gmail.com>
2024-04-21 23:25:37 +0530

    auto checking in

--- 07-functions/ec2.tf
+++ 07-functions/ec2.tf
@@ -1,14 +1,9 @@
-data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "DevOps-LabImage-Centos-8"
-  owners           = ["767397813969"]
-}
 
 # Defining the resources 
 resource "aws_instance" "checker" {
   ami                       = "ami-072983368f2a6eab5" 
   instance_type             = "t3.micro"
-  count                     = 3
+  count                     = 1
 
   tags = {
     Name = "checker-instance-${count.index+1}"
