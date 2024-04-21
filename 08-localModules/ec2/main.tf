03e80a5c09701e116d7ae5cc8cd356a5b60f7eba
GopalIndla <gopal.prnl7@gmail.com>
2024-04-21 19:57:38 +0530

    auto checking in

--- 08-localModules/ec2/main.tf
+++ 08-localModules/ec2/main.tf
@@ -1,12 +1,12 @@
-data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "DevOps-LabImage-Centos-8"
-  owners           = ["355449129696"]
-}
+# data "aws_ami" "ami" {
+ # most_recent      = true
+ # name_regex       = "DevOps-LabImage-Centos-8"
+ # owners           = ["355449129696"]
+# }
 
 # Defining the resources 
 resource "aws_instance" "checker" {
-  ami                       = data.aws_ami.ami.id 
+  ami                       = "ami-072983368f2a6eab5" 
   instance_type             = "t3.micro"
   vpc_security_group_ids    = [var.sg]
   
